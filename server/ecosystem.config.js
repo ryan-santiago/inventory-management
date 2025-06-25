c5184638a14a37743d6e226ad6207fb3782c6e3e
Ryan Santiago <ryan.santiago.ict@gmail.com>
2025-06-26 01:21:12 +0800

    fix ecosystem config 2

--- server/ecosystem.config.js
+++ server/ecosystem.config.js
@@ -1,13 +1,11 @@
 module.exports = {
-	app: [
+	apps: [
 		{
 			name: 'inventory-management',
-			script: '/usr/bin/npm',
-			args: 'run dev',
-			interpreter: 'none',
+			script: './node_modules/.bin/ts-node',
+			args: 'src/index.ts', // or server.ts depending on your structure
 			env: {
 				NODE_ENV: 'development',
-				ENV_VAR1: 'environment-variable',
 			},
 		},
 	],
