fff71b392819de4fb2bf6be238bcc2f32747e4fd
Ryan Santiago <ryan.santiago.ict@gmail.com>
2025-06-26 01:14:10 +0800

    fix interpreter

--- server/ecosystem.config.js
+++ server/ecosystem.config.js
@@ -4,6 +4,7 @@ module.exports = {
 			name: 'inventory-management',
 			script: 'npm',
 			args: 'run dev',
+			interpreter: 'none',
 			env: {
 				NODE_ENV: 'development',
 				ENV_VAR1: 'environment-variable',
